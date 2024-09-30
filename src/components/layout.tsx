3b7f612a10025fa0e0c8d7f2a4c1911742aa83bc
juyesu <dong4757@naver.com>
2024-09-30 10:55:00 +0900

    네이버쇼핑api 관련 코드 삭제 및 server실행 코드 삭제

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,7 +1,23 @@
 import type { ChildrenComponentsProps } from "@/types/type";
 
 const RootLayout = ({ children }: ChildrenComponentsProps) => {
-  return <>{children}</>;
+  return <>
+    <header>
+      <div className="flex justify-between items-center p-4 h-16 border">
+        <h1>로고</h1>
+        <nav>
+          <ul className="flex gap-8">
+            <li>Home</li>
+            <li>Menu</li>
+            <li>About</li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+    <main>
+      {children}
+    </main>
+  </>;
 };
 
 export default RootLayout;
