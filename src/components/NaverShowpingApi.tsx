2317d671ab287b13783b463d8e0707088c21ee0d
juyesu <dong4757@naver.com>
2024-09-30 11:44:44 +0900

    메인 페이지에 타이틀 영역 생성

--- src/components/NaverShowpingApi.tsx
+++ src/components/NaverShowpingApi.tsx
@@ -1,5 +1,12 @@
 const NaverShowpingApi = () => {  
     return <>
+      <div className="h-80 px-8 py-16 bg-slate-400">
+        <div className="w-1/2">
+          <h1 className="mb-4 font-bold text-2xl text-white">AI 컴퓨터 사양 추천</h1>
+          <p className="text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt debitis aspernatur nostrum eos magnam adipisci culpa saepe laudantium tempora, asperiores soluta porro dignissimos</p>
+          <button type="button" className="mt-4 py-3 px-6 text-sm bg-slate-200 rounded-full">추천받으러 가기</button>
+        </div>
+      </div>
     </>;
 };
 
