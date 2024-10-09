e30860b78ba53d4b9f969b20a5a891198f269591
juyesu <dong4757@naver.com>
2024-10-09 14:31:28 +0900

    페이지 너비가 일정 크기 이하일 때 비정상적으로 컨텐츠가 출력되는 문제 수정 (반응형 css지정 lg -> 2xl)

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -2,13 +2,14 @@ import Link from "next/link";
 
 const MainPage = () => {
     return <>
-      <div className="flex items-center h-[32rem] px-8 lg:px-[30rem] py-16 bg-[url('/images/title.jpg')]">
+      <div className="flex items-center h-[32rem] px-8 2xl:px-[30rem] py-16 bg-[url('/images/title.jpg')]">
         <div className="w-1/2">
           <h1 className="mb-4 font-semibold text-4xl text-white">AI 컴퓨터 사양 추천</h1>
           <p className="text-white text-lg">Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt debitis aspernatur nostrum eos magnam adipisci culpa saepe laudantium tempora, asperiores soluta porro dignissimos</p>
           <Link href='PcSpecsRecommendation'>
             <button type="button" className="mt-12 py-4 px-8 text-sm bg-slate-200 rounded-full opacity-80">추천받으러 가기</button>
           </Link>
+          <div></div>
         </div>
       </div>
     </>;
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 const RootLayout = ({ children }: ChildrenComponentsProps) => {
   return <>
     <header>
-      <div className="flex justify-between items-center py-8 px-8 lg:px-[32rem] h-16 border">
+      <div className="flex justify-between items-center py-8 px-8 2xl:px-[32rem] h-16 border">
         <h1><Link href='.'>logo</Link></h1>
         <nav>
           <ul className="flex gap-8">
