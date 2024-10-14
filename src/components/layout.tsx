84af5b59681c116194ba08066c9d1b73834828ff
SeongMoong <sm5414@naver.com>
2024-10-14 10:47:14 +0900

    사진 파일 변환 및 about 페이지 오류 수정
    404에러 해결

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -2,23 +2,27 @@ import type { ChildrenComponentsProps } from "@/types/type";
 import Link from "next/link";
 
 const RootLayout = ({ children }: ChildrenComponentsProps) => {
-  return <>
-    <header>
-      <div className="flex justify-between items-center py-8 px-8 2xl:px-[32rem] h-16 border">
-        <h1><Link href='.'>logo</Link></h1>
-        <nav>
-          <ul className="flex gap-8">
-            <li>Home</li>
-            <li>Menu</li>
-            <li><Link href='About'>About</Link></li>
-          </ul>
-        </nav>
-      </div>
-    </header>
-    <main>
-      {children}
-    </main>
-  </>;
+  return (
+    <>
+      <header>
+        <div className="flex justify-between items-center py-8 px-8 2xl:px-[32rem] h-16 border">
+          <h1>
+            <Link href=".">logo</Link>
+          </h1>
+          <nav>
+            <ul className="flex gap-8">
+              <li>Home</li>
+              <li>Menu</li>
+              <li>
+                <Link href="/about">About</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+      <main>{children}</main>
+    </>
+  );
 };
 
 export default RootLayout;
