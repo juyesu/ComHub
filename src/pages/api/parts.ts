52702a040e7ff086dd068b0c7f4dd1fa3c52ff6d
h0xxln <alxl47@naver.com>
2024-11-18 12:34:12 +0900

    부품 리스트 페이지 쿼리/데이터베이스 연동

---
+++ src/pages/api/parts.ts
@@ -0,0 +1,38 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import sqlite3 from "sqlite3";
+import { open } from "sqlite";
+import path from "path";
+
+const dbPath = path.join(process.cwd(), 'public', 'comhub_db', 'comhubdb1.db');
+ // 절대 경로 사용
+
+// 데이터베이스 연결 함수
+async function openDb() {
+  return open({
+    filename: dbPath,
+    driver: sqlite3.Database,
+  });
+}
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    const db = await openDb();
+
+    if (!db) {
+      console.error("Failed to open database");
+      return res.status(500).json({ error: "Failed to open database" });
+    }
+
+    const parts = await db.all(`
+      SELECT ID as id, 종류 as type, 제조회사 as manufacturer,
+      이름 as name, about, 가격 as price
+      FROM comhubdb2;
+    `);
+
+    res.status(200).json(parts);
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+}
+
