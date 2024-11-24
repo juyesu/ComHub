c21d6249137ac5ccd3ef2d516415faf532bdf136
h0xxln <alxl47@naver.com>
2024-11-24 12:02:48 +0900

    footer 영역 추가

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -4,7 +4,8 @@ import Image from "next/image";
 
 const RootLayout = ({ children }: ChildrenComponentsProps) => {
   return (
-    <>
+    <div className="flex flex-col min-h-screen">
+      {/* Header */}
       <header>
         <div className="flex justify-between items-center py-8 md:px-32 2xl:px-96 min-[1800px]:px-[32rem] h-16 border">
           <Link href=".">
@@ -25,17 +26,43 @@ const RootLayout = ({ children }: ChildrenComponentsProps) => {
                 <Link href="/about">About</Link>
               </li>
               <li>
-                <Link href="/computerParts">ComputerParts</Link>
+                <Link href="/ComputerParts">ComputerParts</Link>
               </li>
               <li>
-                <Link href="/surveyPage">surveyPage</Link>
+                <Link href="/surveyPage">Survey</Link>
               </li>
             </ul>
           </nav>
         </div>
       </header>
-      <main>{children}</main>
-    </>
+
+      {/* Main Content */}
+      <main className="flex-grow">{children}</main>
+
+      {/* Footer */}
+      <footer className="flex justify-between items-center py-8 md:px-32 2xl:px-96 min-[1800px]:px-[32rem] h-16 border">
+        {/* Left Section */}
+        <p className="text-sm font-bold">
+          © 2024 COMHUB - All Rights Reserved.
+        </p>
+
+        {/* Center Section */}
+        <p className="text-sm font-bold">
+          Contact us:{" "}
+          <a
+            href="mailto:comhubyuhan@gmail.com"
+            className="underline hover:text-gray-400"
+          >
+            comhubyuhan@gmail.com
+          </a>
+        </p>
+
+        {/* Right Section */}
+        <p className="text-sm font-bold">
+          Made by: 김호진, 김지혁, 정성문
+        </p>
+      </footer>
+    </div>
   );
 };
 
