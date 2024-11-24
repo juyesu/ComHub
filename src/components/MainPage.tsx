8278ede11a0529f472e43fbd62194252ea1cf50d
h0xxln <alxl47@naver.com>
2024-11-24 17:05:26 +0900

    메인 페이지 pro 모드 버튼 주석처리

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -25,7 +25,7 @@ const MainPage = () => {
                 <h2 className="mt-1 mb-6 font-extrabold text-end text-4xl text-gray-900">Pro 사용자 모드</h2>
                 <p className="text-end md:text-base xl:text-lg text-gray-900">Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt debitis aspernatur nostrum eos magnam adipisci culpa saepe laudantium tempora, asperiores soluta porro dignissimos</p>
                 <Link href='PcSpecsRecommendationProMode'>
-                  <button type="button" className="mt-16 py-4 px-10 text-sm bg-gray-900 hover:bg-gray-900 hover:shadow-md hover:shadow-gray-600 rounded-full font-bold text-white opacity-80">프로모드로 전환</button>
+               {/* <button type="button" className="mt-16 py-4 px-10 text-sm bg-gray-900 hover:bg-gray-900 hover:shadow-md hover:shadow-gray-600 rounded-full font-bold text-white opacity-80">프로모드로 전환</button>*/}
                 </Link>
               </div>
             </div>
